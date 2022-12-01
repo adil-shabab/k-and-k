9ef2d17705f097802a6bd8a7ffb6344300cfcbac
Adil Shabab <adilshabab2004@gmail.com>
2022-12-01 14:42:18 +0530

    featured properties added

--- script.js
+++ script.js
@@ -149,3 +149,95 @@ for (const div of document.getElementById('r').getElementsByTagName('label')) {
   }
 }
 
+
+
+
+// tab 
+(function() {
+    $(function() {
+      var toggle;
+      return toggle = new Toggle('.toggle');
+    });
+  
+    this.Toggle = (function() {
+      Toggle.prototype.el = null;
+  
+      Toggle.prototype.tabs = null;
+  
+      Toggle.prototype.panels = null;
+  
+      function Toggle(toggleClass) {
+        this.el = $(toggleClass);
+        this.tabs = this.el.find(".tab");
+        this.panels = this.el.find(".panel");
+        this.bind();
+      }
+  
+      Toggle.prototype.show = function(index) {
+        var activePanel, activeTab;
+        this.tabs.removeClass('active');
+        activeTab = this.tabs.get(index);
+        $(activeTab).addClass('active');
+        this.panels.hide();
+        activePanel = this.panels.get(index);
+        return $(activePanel).show();
+      };
+  
+      Toggle.prototype.bind = function() {
+        var _this = this;
+        return this.tabs.unbind('click').bind('click', function(e) {
+          return _this.show($(e.currentTarget).index());
+        });
+      };
+  
+      return Toggle;
+  
+    })();
+  
+  }).call(this);
+
+
+
+// owl carousel in property bbox 
+$('.img-box').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+})
+
+
+
+
+//featured slider
+$('.featured-carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    dots: false,
+    arrows: true,
+    infinite: false,
+    responsive: [{
+            breakpoint: 1099,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+    ]
+  });
