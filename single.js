5bb7a7f7326f7039529744c17b51455c68eacb91
Adil Shabab <adilshabab2004@gmail.com>
2022-12-05 16:14:36 +0530

    some more changes added

--- single.js
+++ single.js
@@ -48,6 +48,27 @@ $('.img-box').owlCarousel({
 
 
 
+// inner ad carousel 
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
 
 
 let main_img = document.querySelector('.mainImg')
