ca91b03e7f7553c23d62b8ec0a63e434f10f1902
Adil Shabab <adilshabab2004@gmail.com>
2022-12-05 11:30:42 +0530

    buy page added

---
+++ buy.js
@@ -0,0 +1,13 @@
+// owl carousel in property bbox 
+$('.img-box').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+  })
+  
+  
+  
+  
