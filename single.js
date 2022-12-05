aeea64f7693a518f47e03a95f63e172eeef8bd4d
Adil Shabab <adilshabab2004@gmail.com>
2022-12-05 14:23:03 +0530

    some more changes added

---
+++ single.js
@@ -0,0 +1,46 @@
+$('.property-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+
+
+let main_img = document.querySelector('.mainImg')
+let small_img = document.querySelectorAll('.smallImg')
+
+small_img.forEach(img => img.addEventListener('click', function(){
+    main_img.src = img.src
+}))
